7b1e3fed039344bf68756f7b1ba699e7395ded09
NikolasSnorkell <pushok_nik@mail.ru>
2022-02-16 16:14:04 +0300

    Version 1.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-let repos = [];
+let repos = [],currentElem;
 
 
 
@@ -8,7 +8,9 @@ function repoCardSample(link0,screen0,name0){
     this.screen = screen0;
     this.name = name0;
     this.show = ()=>{
-        $("#repositories_block").html($("#repositories_block").html()+"<div class='repoCard' style='background-image:url("+this.screen+"_blurred.png)'><a  href='"+this.link+"'  target='_blank'><span>"+this.name+"</span></a></div>");
+        $("#repositories_block").html($("#repositories_block").html()+"<div class='repoCard' style='background-image:url("+this.screen+"_blurred.png);opacity:0;transform:translateY(50%);'><a  href='"+this.link+"'  target='_blank'><span>"+this.name+"</span></a></div>");
+     
+    
     }
     this.hovered = (elem) =>{
         $(elem).attr("style","background-image:url("+this.screen+".png)");
@@ -29,21 +31,49 @@ repos.push(new repoCardSample("https://nikolassnorkell.github.io/Memory-Card-Gam
 
 for(let i=0;i<repos.length;i++){
    repos[i].show();
+   
+//    console.log(currentElem[i]);
   
+ 
+   
 };
+
+reposCards = $(".repoCard");
+
+for(let i=0;i<reposCards.length;i++){
+   
+
+ setTimeout(function(){
+    $(reposCards).eq(i).css({opacity:"0.8"});
+     $(reposCards).eq(i).css({transform:"translateY(0%)"});
+    },i+2+"00");
+}
+
+setTimeout(function(){
+$("#snorkell_logo_block").children("img").css({transform:"scale(1) rotate(0deg)",opacity:"1"})
+},500);
+
+// Hover Cards Animation
 
 $(".repoCard").mouseenter(function(){
     repos[$(this).index()].hovered(this);
-    $(this).css({opacity:"1"});    
-     
+    $(this).css({opacity:"1"});  
+    $(this).css({transform:"translateY(0%)"});
+    $(this).children("a").children("span").css("bottom","-50px");
  
 });
 $(".repoCard").mouseleave(function(){
     repos[$(this).index()].unhovered(this);
     $(this).css({opacity:"0.8"});
-
+    $(this).css({transform:"translateY(0%)"});
+    $(this).children("a").children("span").css("bottom","0px");
 });
 
+
+
+
+
+// Version Banner
 setTimeout(()=>{
     $("#version").animate({
         top:"10px"
